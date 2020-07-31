6197c00ca3e252c409d9576b573120934ae78c55
JunQuann <limjunquann@gmail.com>
2020-07-31 22:48:14 +0800

    added list all book feature

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,25 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
+    private final String[] books = new String[] {
+        "Principles: Life and Work",
+        "When Breath Becomes Air",
+        "Why We Sleep: Unlocking the Power of Sleep and Dreams",
+    };
 
     public static void main(String[] args) {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.start();
+        bibliotecaApp.listAllBooks();
+    }
+
+    public void start() {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+    }
+
+    public void listAllBooks() {
+        for (String book : books) {
+            System.out.println(book);
+        }
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,6 +1,8 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -9,13 +11,30 @@ import java.io.PrintStream;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStream() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStream() {
+        System.setOut(originalOut);
+    }
 
     @Test
     public void testWelcomeMessage() {
-        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-        System.setOut(new PrintStream(outContent));
-        BibliotecaApp.main(new String[0]);
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.start();
         assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n", outContent.toString());
+    }
+
+    @Test
+    public void testListAllBooks() {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.listAllBooks();
+        assertEquals("Principles: Life and Work\nWhen Breath Becomes Air\nWhy We Sleep: Unlocking the Power of Sleep and Dreams\n", outContent.toString());
     }
 }
