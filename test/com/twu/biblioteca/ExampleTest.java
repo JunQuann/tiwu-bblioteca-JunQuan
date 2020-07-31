6e513275f66ee09420167fdd04fdc4808c5837c7
JunQuann <limjunquann@gmail.com>
2020-07-31 20:32:51 +0800

    added test case and function for welcome message

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,12 +2,20 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testWelcomeMessage() {
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(outContent));
+        BibliotecaApp.main(new String[0]);
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n", outContent.toString());
     }
 }
